de229811bc0770691790bfee289f0a0e15c67643
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-07-02 13:42:53 +0100

    Update Color.kt
    Reset color values to default ones and also add new custom colors

--- app/src/main/java/dev/hayohtee/quicknote/ui/theme/Color.kt
+++ app/src/main/java/dev/hayohtee/quicknote/ui/theme/Color.kt
@@ -2,12 +2,15 @@ package dev.hayohtee.quicknote.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Brown80 = Color(0xFFFE9A37)
-val Purple80 = Color(0xFF9585BA)
-val LightGreen80 = Color(0xFFCBDB57)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
 
-val Brown40 = Color(0xA9FE9A37)
-val Purple40 = Color(0x9F9585BA)
-val LightGreen40 = Color(0xB9CBDB57)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
 
-val Black = Color(0xFF070706)
+val Orange = Color(0xFFFE9A37)
+val Purple = Color(0xFF9585BA)
+val LightGreen = Color(0xFFCBDB57)
+val DarkRed = Color(0xFFF96A4B)
