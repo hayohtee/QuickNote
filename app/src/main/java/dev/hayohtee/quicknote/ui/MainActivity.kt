0481c9d6d660872b0718260c9bcc5dd969a21d70
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-07-08 16:06:54 +0100

    Update MainActivity

--- app/src/main/java/dev/hayohtee/quicknote/ui/MainActivity.kt
+++ app/src/main/java/dev/hayohtee/quicknote/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.hayohtee.quicknote
+package dev.hayohtee.quicknote.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,12 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.hayohtee.quicknote.ui.theme.QuickNoteTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +21,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    QuickNotesApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuickNoteTheme {
-        Greeting("Android")
     }
 }
