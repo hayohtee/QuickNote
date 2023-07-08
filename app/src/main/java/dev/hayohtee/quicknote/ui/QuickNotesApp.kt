4510b4023ca87e8896250f2bfd51ba7902ff7783
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-07-08 16:07:34 +0100

    Create QuickNotesApp

---
+++ app/src/main/java/dev/hayohtee/quicknote/ui/QuickNotesApp.kt
@@ -0,0 +1,87 @@
+package dev.hayohtee.quicknote.ui
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import dev.hayohtee.quicknote.ui.screens.addnote.AddNoteScreen
+import dev.hayohtee.quicknote.ui.screens.addnote.AddNoteViewModel
+import dev.hayohtee.quicknote.ui.screens.detail.NoteDetailScreen
+import dev.hayohtee.quicknote.ui.screens.detail.NoteDetailViewModel
+import dev.hayohtee.quicknote.ui.screens.editnote.EditNoteScreen
+import dev.hayohtee.quicknote.ui.screens.editnote.EditNoteViewModel
+import dev.hayohtee.quicknote.ui.screens.notes.NoteListScreen
+import dev.hayohtee.quicknote.ui.screens.notes.NotesViewModel
+
+@Composable
+fun QuickNotesApp() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "notes") {
+        composable(route = "notes") {
+            val viewModel: NotesViewModel = hiltViewModel()
+
+            NoteListScreen(
+                state = viewModel.state.value,
+                onSearchClick = { /*TODO*/ },
+                onItemClick = { id ->
+                    navController.navigate("notes/$id")
+                },
+                onAddNoteClick = { navController.navigate(route = "add_note") }
+            )
+        }
+
+        composable(route = "add_note") {
+            val viewModel: AddNoteViewModel = hiltViewModel()
+
+            AddNoteScreen(
+                note = viewModel.state.value,
+                onTitleChange = viewModel.onTitleChange,
+                onContentChange = viewModel.onContentChange,
+                onBackButtonClick = { navController.popBackStack() },
+                saveNote = {
+                    viewModel.addNote()
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable(
+            route = "notes/{note_id}",
+            arguments = listOf(navArgument("note_id") {
+                type = NavType.LongType
+            })
+        ) {
+            val viewModel: NoteDetailViewModel = hiltViewModel()
+
+            NoteDetailScreen(
+                state = viewModel.state.value,
+                onEditNote = { id -> navController.navigate("notes/edit/$id") },
+                onBackPressed = { navController.popBackStack() }
+            )
+        }
+
+        composable(
+            route = "notes/edit/{note_id}",
+            arguments = listOf(navArgument("note_id") {
+                type = NavType.LongType
+            })
+        ) {
+            val viewModel: EditNoteViewModel = hiltViewModel()
+
+            EditNoteScreen(
+                note = viewModel.state.value,
+                onTitleChange = viewModel.onTitleChange,
+                onContentChange = viewModel.onContentChange,
+                onBackButtonClick = { navController.popBackStack() },
+                updateNote = {
+                    viewModel.updateNote()
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
