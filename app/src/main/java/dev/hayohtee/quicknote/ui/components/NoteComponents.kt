0d3b401d54983523eba25bcb53fe95d8459ede3c
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-07-13 17:51:41 +0100

    Remove scrollState from NoteTextField in NoteComponents.kt

--- app/src/main/java/dev/hayohtee/quicknote/ui/components/NoteComponents.kt
+++ app/src/main/java/dev/hayohtee/quicknote/ui/components/NoteComponents.kt
@@ -10,16 +10,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -127,21 +124,13 @@ fun NoteTextField(
     modifier: Modifier = Modifier
 ) {
     Box(modifier = modifier) {
-
-        val scrollState = rememberScrollState()
-        LaunchedEffect(key1 = scrollState.maxValue) {
-            scrollState.scrollTo(scrollState.maxValue)
-        }
-
         BasicTextField(
             value = value,
             onValueChange = onValueChange,
             textStyle = textStyle,
             cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
             keyboardOptions = keyboardOptions,
-            modifier = Modifier
-                .fillMaxWidth()
-                .verticalScroll(scrollState)
+            modifier = modifier.fillMaxWidth()
         )
 
         if (value.isEmpty()) {
@@ -192,7 +181,8 @@ fun NoteFields(
             ),
             textStyle = MaterialTheme.typography.bodyLarge.copy(
                 color = MaterialTheme.colorScheme.onBackground
-            )
+            ),
+            modifier = Modifier.weight(1f)
         )
 
     }
