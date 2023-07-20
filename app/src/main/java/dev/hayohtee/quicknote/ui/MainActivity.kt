968d6855ae1c38f48442996b5799305b57512c0e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-07-20 12:02:55 +0100

    Make decor view to not fit the root-level content view, fix #6

--- app/src/main/java/dev/hayohtee/quicknote/ui/MainActivity.kt
+++ app/src/main/java/dev/hayohtee/quicknote/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import dev.hayohtee.quicknote.ui.theme.QuickNoteTheme
 
@@ -14,6 +15,9 @@ import dev.hayohtee.quicknote.ui.theme.QuickNoteTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             QuickNoteTheme {
                 // A surface container using the 'background' color from the theme
